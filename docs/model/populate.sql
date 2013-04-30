55a4b4e5bf332f804a0e9863b33573db4e81f85c
LucaBa <luca.barazzuol@gmail.com>
2013-04-30 06:14:29 +0200

    populated table event

--- docs/model/populate.sql
+++ docs/model/populate.sql
@@ -1,15 +1,31 @@
+
 DELETE 
-FROM USER;
+FROM ems.event;
+
+DELETE 
+FROM ems.user;
 
 INSERT
-INTO USER(fname,lname,date_of_birth,email,PASSWORD,role)
+INTO ems.user(fname,lname,date_of_birth,email,PASSWORD,role)
 VALUES ('Luca', 'Be', '19910101','lucabelles@gmail.com' ,'password','admin');
 
 INSERT
-INTO USER(fname,lname,date_of_birth,email,PASSWORD,role)
+INTO ems.user(fname,lname,date_of_birth,email,PASSWORD,role)
 VALUES ('Luca', 'Ba', '19710703','luca.barazzuol@gmail.com' ,'password','event_mng');
 
 INSERT
-INTO USER(fname,lname,date_of_birth,email,PASSWORD,role)
+INTO ems.user(fname,lname,date_of_birth,email,PASSWORD,role)
 VALUES ('Alex', 'Stan','19910202','alexstannumberone@gmail.com' ,'password','group_mng');
 
+
+INSERT
+INTO event(id_manager,NAME,description,START,END,enrollment_start,enrollment_end) 
+VALUES ((SELECT id FROM ems.user WHERE role = 'event_mng' LIMIT 1), 'event1', 'description1', '20991231', '20991231', '20991231', '20991231');
+
+INSERT
+INTO event(id_manager,NAME,description,START,END,enrollment_start,enrollment_end) 
+VALUES ((SELECT id FROM ems.user WHERE role = 'event_mng' LIMIT 1), 'event2', 'description2', '20991231', '20991231', '20991231', '20991231');
+
+INSERT
+INTO event(id_manager,NAME,description,START,END,enrollment_start,enrollment_end) 
+VALUES ((SELECT id FROM ems.user WHERE role = 'event_mng' LIMIT 1), 'event3', 'descriptio3', '20991231', '20991231', '20991231', '20991231');
