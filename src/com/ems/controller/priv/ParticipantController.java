165874e57c9e218d03261d62bc1b5685d77c9b9d
LucaBa <luca.barazzuol@gmail.com>
2013-05-13 22:37:32 +0200

    javamail

--- src/com/ems/controller/priv/ParticipantController.java
+++ src/com/ems/controller/priv/ParticipantController.java
@@ -1,6 +1,18 @@
 package com.ems.controller.priv;
 
 import java.io.IOException;
+import java.util.Properties;
+
+import javax.mail.AuthenticationFailedException;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,14 +20,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.net.*;
 import org.apache.log4j.Logger;
 
-import com.ems.dao.EventDao;
+
 import com.ems.dao.GroupDao;
+
 import com.ems.dao.ParticipantDao;
-import com.ems.model.Event;
-import com.ems.model.Group;
 import com.ems.model.Participant;
+import com.sun.mail.smtp.SMTPSaslAuthenticator;
 
 
 /**
@@ -25,7 +41,8 @@ import com.ems.model.Participant;
 		"/private/participantList.html", 
 		"/private/participant.jsp", 
 		"/private/participantAdd",
-		"/private/participantDelete"
+		"/private/participantDelete",
+		"/private/participantInvite"		
 		})
 public class ParticipantController extends HttpServlet {
 	
@@ -137,46 +154,136 @@ public class ParticipantController extends HttpServlet {
     		id_group = Integer.parseInt(request.getParameter("id_group").toString());
     	}
     	
-    	log.debug("----------------> id_group: " + request.getParameter("id_group"));
-    	
-		record.setId_group(id_group);
-    	record.setFname(request.getParameter("fname"));
-    	record.setLname(request.getParameter("lname"));
-    	record.setDate_of_birth(request.getParameter("date_of_birth"));
-    	record.setRegistration_date(request.getParameter("registration_date"));
-    	record.setApproved(Boolean.parseBoolean(request.getParameter("approved")));
-    	record.setBlocked(Boolean.parseBoolean(request.getParameter("blocked")));
-    	
-    	String id = request.getParameter("id");
-        
-    	log.debug("id: " + id);
-    	
-        if(id == null || id.isEmpty()) {
-        	log.debug("INSERT");
-            dao.addRecord(id_group, record);
-            request.setAttribute("id_group", id_group);
-        }
-        else
-        {
-        	log.debug("UPDATE");
-        	record.setId(Integer.parseInt(id));
-            dao.updateRecord(record);
-            request.setAttribute("id_group", id_group);
-        }
-        
-        GroupDao gd = new GroupDao();
-        request.setAttribute("groups", gd.getAllRecords());
-        
-        String forward = "/WEB-INF/jsp/private" + LIST_USER;
-        log.debug("forward: " + forward);
-        request.setAttribute("records", dao.getAllRecordsById_group(id_group));
-		try {
-			getServletConfig().getServletContext().getRequestDispatcher(forward).forward(request, response);
-			} 
-		catch (Exception ex) {
-				ex.printStackTrace();
-			}
+    	if (request.getParameter("action") == null) {
+    		//form for INSERT or UPDATE
+	    	
+	    	log.debug("----------------> id_group: " + request.getParameter("id_group"));
+	    	
+			record.setId_group(id_group);
+	    	record.setFname(request.getParameter("fname"));
+	    	record.setLname(request.getParameter("lname"));
+	    	record.setDate_of_birth(request.getParameter("date_of_birth"));
+	    	record.setRegistration_date(request.getParameter("registration_date"));
+	    	record.setApproved(Boolean.parseBoolean(request.getParameter("approved")));
+	    	record.setBlocked(Boolean.parseBoolean(request.getParameter("blocked")));
+	    	
+	    	String id = request.getParameter("id");
+	        
+	    	log.debug("id: " + id);
+	    	
+	        if(id == null || id.isEmpty()) {
+	        	log.debug("INSERT");
+	            dao.addRecord(id_group, record);
+	            request.setAttribute("id_group", id_group);
+	        }
+	        else
+	        {
+	        	log.debug("UPDATE");
+	        	record.setId(Integer.parseInt(id));
+	            dao.updateRecord(record);
+	            request.setAttribute("id_group", id_group);
+	        }
+	        
+	        GroupDao gd = new GroupDao();
+	        request.setAttribute("groups", gd.getAllRecords());
+	        
+	        String forward = "/WEB-INF/jsp/private" + LIST_USER;
+	        log.debug("forward: " + forward);
+	        request.setAttribute("records", dao.getAllRecordsById_group(id_group));
+			try {
+				getServletConfig().getServletContext().getRequestDispatcher(forward).forward(request, response);
+				} 
+			catch (Exception ex) {
+					ex.printStackTrace();
+				}
+    	}
+    	else if (request.getParameter("action").equals("invite") ){
+    		//invite participant
+    		log.debug("Invite Participant");
+    		
+
+    		String from = "cestino@gmail.com";
+            String to = request.getParameter("to");
+    		log.debug("TO: " + to);
+            String subject = "Invitation";
+            String message = "sdjnfkasjdnf aksdjf ask dfasdk fasdk fk2";;
+            String login = "cestino@gmail.com";
+            String password = "01abekul";
+    		
+    		
+    		
+            try {
+            	log.debug("try sending");
+                Properties props = new Properties();
+                
+
+                props.put("mail.smtp.starttls.enable", "true"); // added this line
+                props.put("mail.smtp.host", "smtp.googlemail.com");
+                props.put("mail.smtp.user", from);
+                props.put("mail.smtp.password", password);
+                props.put("mail.smtp.port", "587");
+                props.put("mail.smtp.auth", "true");
+                
+        		/*props.put("mail.smtp.auth", "true");
+        		props.put("mail.smtp.starttls.enable", "true");
+        		props.put("mail.smtp.host", "smtp.googlemail.com");
+        		props.put("mail.smtp.port", "465");
+        		*/
+        		
+                //props.setProperty("mail.smtp.host", "smtp.gmail.com");
+                //props.setProperty("mail.smtp.port", "587");
+              //  props.setProperty("mail.smtp.auth", "true");
+        		//props.setProperty("mail.smtp.starttls.enable", "true");
+
+            	log.debug("1");
+                Authenticator auth = new SMTPAuthenticator(login, password);
+            	log.debug("2");
+                Session session = Session.getInstance(props, auth);
+            	log.debug("3");
+                MimeMessage msg = new MimeMessage(session);
+            	log.debug("4");
+                msg.setText(message);
+                msg.setSubject(subject);
+                msg.setFrom(new InternetAddress(from));
+                msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            	log.debug("5");
+                Transport.send(msg);
+            	log.debug("6");
+
+            } catch (AuthenticationFailedException ex) {
+            	log.debug("AuthenticationFailedException");
+                request.setAttribute("ErrorMessage", "Authentication failed");
+
+
+
+            } catch (AddressException ex) {
+            	log.debug("AddressException");
+                request.setAttribute("ErrorMessage", "Wrong email address");
+
+
+
+            } catch (MessagingException ex) {
+            	log.debug("MessagingException");
+                request.setAttribute("ErrorMessage", ex.getMessage());
+
+            }
+
+    	}
     	log.trace("END");
 	}
+	
+	//http://zetcode.com/tutorials/jeetutorials/sendingemail/
+    private class SMTPAuthenticator extends Authenticator {
+
+        private PasswordAuthentication authentication;
+
+        public SMTPAuthenticator(String login, String password) {
+            authentication = new PasswordAuthentication(login, password);
+        }
+
+        protected PasswordAuthentication getPasswordAuthentication() {
+            return authentication;
+        }
+    }
 
 }
