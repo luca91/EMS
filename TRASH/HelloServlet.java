2d700a5c8f72f7aeec11af5d085ff76e26816a48
LucaBa <luca.barazzuol@gmail.com>
2013-06-03 16:55:28 +0200

    cleanup

---
+++ TRASH/HelloServlet.java
@@ -0,0 +1,70 @@
+package com.ems.test.general;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+ 
+/**
+* Servlet implementation class HelloServlet
+*/
+public class HelloServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+ 
+    /**
+	 * @uml.property  name="log"
+	 * @uml.associationEnd  multiplicity="(1 1)"
+	 */
+    private Log log = LogFactory.getLog(HelloServlet.class);
+ 
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+ 
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+ 
+        response.setContentType("text/html");
+        log.info("doGet");
+ 
+        String name = request.getParameter("name");
+        String value = request.getParameter("value");
+ 
+        log.debug(name);
+        log.debug(value);
+ 
+        response.getWriter().write("name:" + name + ",value:" + value);
+    }
+ 
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        log.debug("doPost");
+ 
+        String name = request.getParameter("name");
+        String value = request.getParameter("value");
+ 
+        log.debug(name);
+        log.debug(value);
+ 
+        response.getWriter().write("name:" + name + ",value:" + value);
+ 
+    }
+ 
+}
